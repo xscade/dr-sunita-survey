2800319d1f98f478e2e005a969b26706a8f5dc91
xscade <xscade@gmail.com>
2025-11-27 15:15:40 +0530

    Added Extra Slide Fix

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { ProgressBar } from './components/ProgressBar';
 import { Button } from './components/Button';
 import { OptionCard } from './components/OptionCard';
